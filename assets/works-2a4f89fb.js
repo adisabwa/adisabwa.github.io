21aa2a1f564980fd0253ded82a1742ec83e6e1b0
Adi Sabwa <adi.sabwa@gmail.com>
2023-05-23 09:40:30 +0700

    Updates

---
+++ assets/works-2a4f89fb.js
@@ -0,0 +1,36 @@
+import{b as i}from"./main-30a51cf1.js";console.log(i);let a={mengajar:{image:i+"assets/images/logo-ugm.png",title:"Layanan Mengajar",subtitle:"Work at FT UGM",contentMini:"An app that help faculty member's to calculate lecturer's honor according to presence data",content:`<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>`,tech:["CodeIginiter","Vue.js","Tailwind CSS"],link:"https://sms.ft.ugm.ac.id/mengajar",images:[{alt:"Login Page",link:i+"assets/images/authback.jpeg"},{alt:"Login Page",link:i+"assets/images/authback.jpeg"},{alt:"Login Page",link:i+"assets/images/authback.jpeg"}]},workshop:{image:i+"assets/images/logo-ugm.png",title:"SiBuK (Sistem Rembugan Kantor)",subtitle:"Work at FT UGM",contentMini:`Created to help faculty members in processing datas of office activity, creating required documents
+			and paying honor and invoice`,content:`<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>`,tech:["CodeIginiter","Vue.js","Tailwind CSS"],link:"https://sms.ft.ugm.ac.id/mengajar",images:[{alt:"Login Page",link:i+"assets/images/authback.jpeg"},{alt:"Login Page",link:i+"assets/images/authback.jpeg"},{alt:"Login Page",link:i+"assets/images/authback.jpeg"}]},"kegiatan-mahasiswa":{image:i+"assets/images/logo-ugm.png",title:"Pengajuan Kegiatan Mahasiswa",subtitle:"Work at FT UGM",contentMini:`Created to help faculty members in processing datas of office activity, creating required documents
+			and paying honor and invoice`,content:`<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>`,tech:["CodeIginiter","Vue.js","Tailwind CSS"],link:"https://sms.ft.ugm.ac.id/kegiatan-mahasiswa",images:[{alt:"Login Page",link:i+"assets/images/authback.jpeg"},{alt:"Login Page",link:i+"assets/images/authback.jpeg"},{alt:"Login Page",link:i+"assets/images/authback.jpeg"}]}};export{a as w};
